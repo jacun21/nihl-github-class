b70351e376edc0c302d45602c9ed43cdb455257b
Jason <jacunha14@gmail.com>
2023-06-13 11:00:39 -0400

    This is a new test script to learn how to use Github, Git, and R studio

---
+++ test_script_gihubclass.R
@@ -0,0 +1,13 @@
+# Importing our Data
+
+library(tidyverse)
+library(gapminder)
+read_data <- gapminder
+
+# Let us add some folders
+dir.create("images")
+dir.create("docs")
+dir.create("figures")
+
+# Do a quick image search for p53
+# Download the image and move it into the new images folder that you just created
